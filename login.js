2a1f0a808f0880c339320c59700c074f9f1e2955
Giovanna Fiuza <94703688+giifiuza@users.noreply.github.com>
2023-04-19 16:42:03 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,20 @@
+function Login() {
 
+    var usuario = document.getElementById('name').value;
+    usuario=usuario.toLowerCase();
+    var senha= document.getElementById('password').value;
+    senha=senha.toLowerCase();
+    if (usuario=="admin" && senha=="admin") {
+        window.location.href = "/telaCadastro.html"
+
+         if(usuario == "" || senha == "") { 
+             alert("Os campos usuário e senha são obrigatório"); }
+             
+              if(usuario =! "admin" || senha != "admin"){
+                alert("Usuário não cadastrado!")
+             }
+             else (usuario=="admin" && senha=="admin");
+                 alert("Login realizado com sucesso!")
+
+             }
+            }
