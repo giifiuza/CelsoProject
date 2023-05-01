0cf37ba41c982b5dcbcb860b7a01cb2483f2d2c6
Giovanna Fiuza <94703688+giifiuza@users.noreply.github.com>
2023-05-01 20:59:23 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,44 +1,46 @@
 async function Login() {
 
     var usuario = document.getElementById('name').value;
-    usuario=usuario.toLowerCase();
-    var id= document.getElementById('id').value;
-    id=id.toLowerCase();
-    if (usuario !="" && id !="") {
-          
-             const request = {
-                 "nome": usuario,
-                 "id": id
-             }
-             
-             const header = {
-                 method: 'POST',
-                 headers: {
-                     'Accept': 'application/json',
-                     'Content-Type': 'application/json'
-                 },
-                 body: JSON.stringify(request)
-             }
-             
-             console.log(header)
-             
-             await fetch('https://access-system.azurewebsites.net/post', header)
-             .then((Response) => Response.json())
-             .then((data) => {
-                 console.log(data)
-             })
-            Swal.fire({
-                icon: 'success',
-                title: 'cadastro realizado',
-                showConfirmButton: false,
-                timer: 1500
-              })
-            
-            
-              
-             
+    usuario = usuario.toLowerCase();
+    var id = document.getElementById('id').value;
+    id = id.toLowerCase();
+    if (usuario != "" && id != "") {
+        const request = {
+            "nome": usuario,
+            "id": id
+        }
 
-             }
-            }
-        
-        
+        const header = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        }
+
+        console.log(header)
+
+        await fetch('https://access-system.azurewebsites.net/post', header)
+            .then((Response) => Response.json())
+            .then((data) => {
+                console.log(data)
+            })
+        Swal.fire({
+            icon: 'success',
+            title: 'cadastro realizado',
+            showConfirmButton: false,
+            timer: 1500
+        })
+
+    }else if(usuario.length === 1 && id.length === 1 && !isNaN(id)){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Wrong user!',
+            footer: 'Please try again!'
+          })
+    }
+}
+
+
